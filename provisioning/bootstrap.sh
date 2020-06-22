f0aa8b1f1a70c1c90ce8e355793d6c3073a3ceb9
githubfoam <lequestionis@gmail.com>
2020-06-22 22:11:54 +0300

    Вы мо́жете прийти́ на час ра́ньше

--- provisioning/bootstrap.sh
+++ provisioning/bootstrap.sh
@@ -8,4 +8,9 @@ echo "         \   ^__^                                                         
 echo "          \  (oo)\_______                                                          "
 echo "             (__)\       )\/\                                                      "
 echo "                 ||----w |                                                         "
-echo "                 ||     ||    
+echo "                 ||     ||
+
+export WD="/scratch"
+mkdir ${WD} && cd ${WD} 
+apt-get update && apt-get upgrade -qqy && echo 2020-06-22
+apt-get -qqy install build-essential git bison flex gawk cmake swig libssl1.0-dev libmaxminddb-dev libpcap-dev python-dev libcurl4-openssl-dev wget libncurses5-dev ca-certificates zlib1g-dev --no-install-recommends
