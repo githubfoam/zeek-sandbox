a51d624b41ea53e7c13cebbaadb97666c3c0ee61
githubfoam <lequestionis@gmail.com>
2020-06-23 18:53:55 +0300

    из 19-ого ве́ка

--- provisioning/bootstrap.sh
+++ provisioning/bootstrap.sh
@@ -14,7 +14,10 @@ echo "==============================zeek========================================
 export WD="/scratch"
 mkdir ${WD} && cd ${WD}
 apt-get update && apt-get upgrade -qqy && echo 2020-06-22
-apt-get -qqy install build-essential git bison flex gawk cmake swig libssl1.0-dev libmaxminddb-dev libpcap-dev python-dev libcurl4-openssl-dev wget libncurses5-dev ca-certificates zlib1g-dev --no-install-recommends
+
+#Package 'libssl1.0-dev' has no installation candidate
+#apt-get -qqy install build-essential git bison flex gawk cmake swig libssl1.0-dev libmaxminddb-dev libpcap-dev python-dev libcurl4-openssl-dev wget libncurses5-dev ca-certificates zlib1g-dev --no-install-recommends
+apt-get -qqy install build-essential git bison flex gawk cmake swig libmaxminddb-dev libpcap-dev python-dev libcurl4-openssl-dev wget libncurses5-dev ca-certificates zlib1g-dev --no-install-recommends
 
 # Build bro
 export VER="3.0.0"
